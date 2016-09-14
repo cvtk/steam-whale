8e2b28bb474bfc52de672b6276e28ed2a94fa32c
cvtk <olegcvetkov76@gmail.com>
2016-09-14 14:29:51 +0300

    Revert "2"
    This reverts commit f183672596c4dbd2a41f3c52317b847a395aaab0.

--- gruntfile.js
+++ gruntfile.js
@@ -14,9 +14,9 @@ module.exports = function(grunt) {
             css: {
                 src: [
                     'dev/css/*.css',
-                    'dev/css/**/*.css'
+                    'dev/css/**/*.js'
                 ],
-                dest: 'assets/a.css',
+                dest: 'assets/a. css',
             },
         },
 
@@ -24,28 +24,11 @@ module.exports = function(grunt) {
             js: {
                 src: 'assets/a.js',
                 dest: 'assets/a.js'
-            }
-        },
-
-        uncss: {
-            dist: {
-                files: {
-                    'assets/a.css': ['index.html']
-                }
-            }
-        },
-
-        cssmin: {
-            options: {
-                shorthandCompacting: false,
-                roundingPrecision: -1,
-                keepSpecialComments: 0
             },
-            target: {
-                files: {
-                    'assets/a.css': ['assets/a.css']
-                }
-            }
+            css: {
+                src: 'assets/a.css',
+                dest: 'assets/a.css'
+            },
         },
 
         imagemin: {
@@ -70,22 +53,23 @@ module.exports = function(grunt) {
 
         watch: {
             assets: {
-                files: ['index.html', 'dev/js/*.js', 'dev/js/**/*.js', 'dev/css/*.css', 'dev/css/**/*.css'],
+                files: ['index.html', 'dev/js/*.js', 'dev/js/**/*.js', 'dev/css/*.css', 'dev/css/**/*.js'],
                 tasks: ['concat'],
                 options: {
                     spawn: false,
-                    livereload: true
+                    livereload: {
+                        host: 'localhost',
+                        port: 9000
+                    }
                 },
             },
         }
     });
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-uncss');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'uncss', 'cssmin']);
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
     grunt.registerTask('dev', ['connect', 'watch']);
 };
