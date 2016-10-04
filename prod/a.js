438c4f834fe7ce91c5e092ed187cc1c4225f0d3c
cvtk <olegcvetkov76@gmail.com>
2016-10-04 22:01:49 +0300

    2201

--- prod/a.js
+++ prod/a.js
@@ -22,12 +22,10 @@ $(function() {
 	app.minicartWidget.triggers = $("[data-minicart-trigger]");
 
 	app.minicartWidget.toggle = function() {
-		$('#overlay').toggle(550);
-		app.minicartWidget.toggleClass('_hided');
+		app.minicartWidget.toggleClass('_hidden');
 	}
 
 	app.minicartWidget.init = function() {
-		$('body').append('<div id="overlay" style="position: fixed; visibility:hidden; top: 0; left: 0; height: 100%; width: 100%; z-index: 3; background-color: rgba(0,0,0,0.75);"></div>');
 		app.minicartWidget.triggers.each(function() {
 			$(this).click( app.minicartWidget.toggle )
 		})
