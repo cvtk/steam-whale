477c9a26720160e87a53614f6198c86723387d67
cvtk <olegcvetkov76@gmail.com>
2016-11-24 15:44:16 +0300

    12

--- dev/last.js
+++ dev/last.js
@@ -17,6 +17,20 @@ $(function() {
 			});
 		})
 	}
+
+	miniShop2.Order = {
+		getcost: function() {
+			var callbacks = miniShop2.Order.callbacks;
+			callbacks.getcost.response.success = function(response) {
+				$(miniShop2.Order.orderCost).text(miniShop2.Utils.formatPrice(response.data['cost']));
+			}
+			var data = {};
+			data[miniShop2.actionName] = 'order/getcost';
+			miniShop2.send(data, miniShop2.Order.callbacks.getcost, miniShop2.Callbacks.Order.getcost);
+		}
+	}
+	
+
 	miniShop2.Cart.totalCount = '.cart-count';
 	miniShop2.Cart.totalCost = '.summary-price > .price';
 	miniShop2.Message.close = function() {};
